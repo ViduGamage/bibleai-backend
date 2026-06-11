a4702e38a548babede62f157e7d7f2fce51a5661
ViduGamage <vidugamage2003@gmail.com>
2026-06-11 19:55:48 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 app.post('/ask', async (req, res) => {
